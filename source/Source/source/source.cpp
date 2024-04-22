e5f08fe40d13fcd6acc2173f8341e395d7dd0fad
Matias Lecaros <m.lecaros.aranis@gmail.com>
2024-04-22 12:09:33 -0400

    first project commit

---
+++ source/Source/source/source.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "source.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, source, "source" );
